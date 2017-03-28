56922673cc02a26c77c85676648cc198902b1ef7
Jason Simmons <celetavaro@gmail.com>
2017-03-28 01:45:58 -0500

    a skeleton main to be filled in later

---
+++ src/viewqueue/Main.java
@@ -0,0 +1,14 @@
+package viewqueue;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+
+
+
+
+        // main will be one of the last things to be added for server side code
+
+    }
+}
