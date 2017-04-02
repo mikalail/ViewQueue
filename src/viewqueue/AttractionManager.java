f66c0c8e4f4dce66000ac21e59f4039d36976bf9
Jason Simmons <celetavaro@gmail.com>
2017-04-02 05:20:52 -0500

    added an extra display method that includes waits. will need future mod.

--- src/viewqueue/AttractionManager.java
+++ src/viewqueue/AttractionManager.java
@@ -29,7 +29,7 @@ public class AttractionManager {
     }
 
     /**
-     * removes first occurrence of object in list - if present
+     * removes Attraction object from list - if present
      * @param a - an Attraction object
      * @return a boolean result based on success(true) or failure(false)
      */
@@ -41,15 +41,29 @@ public class AttractionManager {
 
     /**
      *displays a simple list of Attractions (name only)
-     * @param a - an Attraction object
+     *
      */
-   public void display(Attraction a){
+    public void display(){
 
        for(Attraction at : list){
 
            System.out.println(at.getName());
 
        }
+   }
+
+    /**
+     * displays each Attraction in list with Name + wait time only.
+     */
+
+   public void waitList(){
+
+        for(Attraction at : list){
+
+            // this should ultimately create a String listing of each to go through the network stream
+            // for now, just printing out with a system message
+            System.out.println(at.toString());
+        }
    }
 
 }
