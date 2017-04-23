0064ae78f8191ac42442961472881eded2f8a688
Luisg9121 <Luisg_91@hotmail.com>
2017-04-23 18:40:05 -0500

    Update Main.java

--- src/viewqueue/Main.java
+++ src/viewqueue/Main.java
@@ -1,9 +1,11 @@
-package viewqueue;
+//package viewqueue;
 
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        AttractionManager attManager = new AttractionManager();
+        AttractionServer s1 = new AttractionServer(attManager);
 
 
 
